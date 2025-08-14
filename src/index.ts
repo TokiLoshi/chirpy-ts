ce388930b5eb522ad8b8b75b5437eae0e8c82c32
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-08-14 15:43:19 -0700

    adding middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { handlerReadiness } from "./api/readiness";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./middleware.js";
 
 const app = express();
 const PORT = 8080;
 app.use(express.text());
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import type { NextFunction, Request, Response } from "express";
+
+type Middleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => Promise<void>;
+
+export const middlewareLogResponses: Middleware = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	res.on("finish", () => {
+		console.log("Middleware");
+		// Grab status code from response object
+		const statusCode = res.statusCode;
+		const method = req.method;
+		const url = req.url;
+		// if the status is non-ok log [NON-OK] <http_method> <url> - Status: <status_code>
+		if (statusCode < 200 || statusCode >= 300) {
+			// replace http_method, url and status code with those properites
+			console.log(`[NON-OK] ${method} ${url} - Status: ${statusCode}`);
+		}
+	});
+
+	// run server
+	// tee the output to server.log
+	// gitignore the server log
+	next();
+};
