54b31f761d1d65407757ae9451605dc4d477f1b3
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-08-17 16:27:43 -0700

    refactoring for admin

---
+++ src/admin/metrics/adminMetrics.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { config } from "../../config.js";
+
+export function adminMetrics(_: Request, res: Response) {
+	res.set("Content-Type", "text/html; charset=utf-8");
+	const count = config.fileserverHits;
+	res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${count} times!</p>
+  </body>
+</html>`);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses, middlewareMetrics } from "./middleware.js";
 import { metricsCounter } from "./api/metrics/count.js";
-import { resetMetrics } from "./api/reset/resetMetrics.js";
+import { resetMetrics } from "./admin/reset/resetMetrics.js";
+import { adminMetrics } from "./admin/metrics/adminMetrics.js";
 
 const app = express();
 const PORT = 8080;
@@ -12,7 +13,8 @@ app.use(middlewareLogResponses);
 app.use("/app", middlewareMetrics, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 app.get("/api/metrics", metricsCounter);
-app.get("/api/reset", resetMetrics);
+app.get("/admin/metrics", adminMetrics);
+app.get("/admin/reset", resetMetrics);
 
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
