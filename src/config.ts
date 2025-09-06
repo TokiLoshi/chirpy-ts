4b9eda022786990c58dc539f476738eab9c90b79
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-09-06 15:21:59 -0700

    adding drizzle queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,16 @@
+import { env } from "node:process";
+
+process.loadEnvFile();
+const envData = process.env;
+
+console.log("connection string I hope: ", envData.DB_URL);
+
 type APIConfig = {
 	fileserverHits: number;
+	dbURL: string;
 };
 
 export const config: APIConfig = {
 	fileserverHits: 0,
+	dbURL: envData.DB_URL || "",
 };
